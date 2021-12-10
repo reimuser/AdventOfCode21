2f5988b7aca52536faa6c30d987cf2af85f93194
Mady Magdalena Reimers <mady.reimers@stud.uni-due.de>
2021-12-10 15:44:10 +0100

    Day2

---
+++ Tag2/day2b.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<array>
+#include<vector>
+#include<sstream>
+
+struct COMMANDS {   // Declare PERSON struct type
+    std::string direction;
+    int unit;
+} command;
+
+
+COMMANDS splitString(std::string line)
+{
+    struct COMMANDS command;
+    std::string delimiter = " ";
+    std::vector<std::string> strings;
+    std::istringstream lineStream(line);
+    std::string str;
+
+    while (getline(lineStream, str, ' ')) 
+    {
+        strings.push_back(str);
+    }
+
+    command.direction =  strings.at(0);
+    command.unit = std::stoi(strings.at(1));
+
+    return command;
+}
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main ()
+{
+    int depth = 0, horizon = 0, aim = 0;
+    std::ifstream input;
+    std::vector<COMMANDS> values;
+
+    input.open("input.txt");
+    std::string line;
+
+    if(input.is_open()) 
+    {
+        while(input)
+        {
+            std::getline(input, line);
+            values.push_back(splitString(line));
+        }
+        values.pop_back();
+
+        for(auto& command : values)
+        {
+            if(command.direction == "forward")
+            {
+                horizon += command.unit;
+                depth += (aim * command.unit);
+            }
+            else if(command.direction == "down")
+            {
+                aim += command.unit;
+            }
+            else if(command.direction == "up")
+            {
+                aim -= command.unit;
+            }
+        }
+
+        std::cout << "depth: " << depth << " horizon: "<< horizon << " mult: " << depth * horizon << std::endl;
+    }
+    else
+    {
+        std::cout << "Couldn't open file\n";
+    }
+
+    return 0;
+}
